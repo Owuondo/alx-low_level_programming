ae65d672303ec6276741d4191a3bb80c4e299f77
Owuondo <victorowuondo7@gmail.com>
2022-06-02 12:23:41 -0700

    my first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Function starts
+ *
+ *   *
+ *
+ *    * Return: Function closes
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}

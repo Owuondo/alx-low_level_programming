e1eb2b4c0e00a262a4e81a532cf7069772ec6ed1
Owuondo <victorowuondo7@gmail.com>
2022-06-05 03:34:24 -0700

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - Prints out a string
  *
- *  * main - beg
- *
- *   *
- *
- *    * Return: stop
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	return (0);
 }

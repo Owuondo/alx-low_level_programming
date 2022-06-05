da98044ee76d124958cdbdb76afbb7db85876b3b
Owuondo <victorowuondo7@gmail.com>
2022-06-05 03:29:22 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - Function starts
+ * main - Prints "Programming is like building a multilingual
+ * puzzle, followed by a new line."
  *
- * Return: Function closes
+ * Return: Always 0.
  */
 int main(void)
 {
-	puts("\"programming is like building multilingual puzzle");
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
